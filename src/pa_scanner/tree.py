61455e6acd7ac06834f5fd7ce5682140db870ab8
sharbouli <sharbouli@myges.fr>
2025-06-27 23:08:12 +0200

    wip

---
+++ src/pa_scanner/tree.py
@@ -0,0 +1,50 @@
+from typing import Self, List
+from anytree import RenderTree, NodeMixin, find
+
+
+class WebNode:
+    path: str = ""
+    protected: bool = False
+    vuln_found: List[str] = []
+    params = {}
+
+
+class WebTree(WebNode, NodeMixin):
+    def __init__(self, path: str, parent=None, children: List[Self] = None):
+        super(WebNode, self).__init__()
+        self.parent = parent
+        self.path = path
+        if children:
+            self.children = children
+
+    def __str__(self):
+        return self.path if not self.protected else f"{self.path} / Protected"
+
+    def __repr__(self):
+        return self.path if not self.params or len(self.params) == 0 else f"{self.path} / has params"
+
+    def find_by_path(self, path: str) -> Self | None:
+        node: Self = None
+        if not path:
+            return node
+        for p in path.strip("/").split("/"):
+            node = find(self if node == None else node,
+                        lambda x: x.path == p, maxlevel=2)
+            if node == None:
+                return node
+        return node
+
+    def add(self, path: str) -> Self | None:
+        if not path:
+            return None
+        path_comp = path.strip("/").split("/")
+        parent = self
+        for p in path_comp[:-1]:
+            node = parent.find_by_path(p)
+            if node == None:
+                node = WebTree(p, parent=parent)
+            parent = node
+        node = parent.find_by_path(path_comp[-1])
+        if node:
+            return node
+        return WebTree(path_comp[-1], parent=parent)
---
+++ src/pa_scanner/utils.py
@@ -0,0 +1,35 @@
+import string
+import random
+from urllib.parse import urlparse, urlunparse, parse_qs, urlencode, urlunparse
+from typing import List
+
+
+def add_or_update_url_param(url: str, key: str, value) -> str:
+    parsed = urlparse(url)
+    query = parse_qs(parsed.query)
+
+    # Update or add the parameter
+    query[key] = value
+
+    # Build new query string
+    new_query = urlencode(query, doseq=True)
+
+    # Construct updated URL
+    new_url = urlunparse(parsed._replace(query=new_query))
+    return new_url
+
+
+def strip_url_params(url: str) -> str:
+    parsed = urlparse(url)
+    stripped = parsed._replace(query="", params="", fragment="")
+    return urlunparse(stripped)
+
+
+def generate_random_value(length=8, digits=False):
+
+    if digits:
+        chars = string.digits
+    else:
+        chars = string.ascii_letters + string.digits
+
+    return ''.join(random.choices(chars, k=length))
